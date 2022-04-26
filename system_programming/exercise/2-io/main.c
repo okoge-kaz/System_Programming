eb715124d8f911a263f5e70420fb636d1891fee1
kazuki <kaz.tokyo.tech20@gmail.com>
2022-04-27 01:05:57 +0900

    tmp

--- system_programming/exercise/2-io/main.c
+++ system_programming/exercise/2-io/main.c
@@ -3,10 +3,10 @@
 #include <fcntl.h>     /* open */
 #include <stdio.h>     /* fopen, fread, fwrite, fclose */
 #include <stdlib.h>    /* exit, atoi */
+#include <string.h>    /* strlen */
 #include <sys/types.h> /* read */
 #include <sys/uio.h>   /* read, write */
 #include <unistd.h>    /* getopt, read, write, close */
-
 /** argv[0] */
 char *cmdname;
 
@@ -20,14 +20,81 @@ int do_cat(int size, char **args) {
     for (int i = 0; i < size; i++) {
         char *fname = args[i];
         LOG("filename = %s", fname);
-        if (!use_stdio) {
-            /* -l unavailable (Task 1); implement here */
-            int file_discripter;
-            if ((file_discripter = open(fname, O_RDONLY)) == -1) {
-                perror("open");
-                exit(1);
+        if (strcmp(fname, "-") == 0) {
+            if (use_stdio) {
+                char buf[buffer_size];
+                int n;
+                while ((n = fread(buf, 1, buffer_size, stdin)) > 0) {
+                    write(STDOUT_FILENO, buf, n);
+                }
+            } else {
+                char buf[buffer_size];
+                int n;
+                while ((n = read(STDIN_FILENO, buf, buffer_size)) > 0) {
+                    write(STDOUT_FILENO, buf, n);
+                }
             }
+        } else {
+            if (!use_stdio) {
+                /* -l unavailable (Task 1); implement here */
+                int file_discripter;
+                if ((file_discripter = open(fname, O_RDONLY)) == -1) {
+                    perror("open");
+                    exit(1);
+                }
 
+                char *buffer = malloc(buffer_size);
+                if (buffer == NULL) {
+                    perror("malloc");
+                    exit(1);
+                }
+
+                int n;
+                while ((n = read(file_discripter, buffer, buffer_size)) > 0) {
+                    if (write(STDOUT_FILENO, buffer, n) != n) {
+                        perror("write");
+                        exit(1);
+                    }
+                };
+
+                free(buffer);
+                if (close(file_discripter) == -1) {
+                    perror("close");
+                    exit(1);
+                };
+            } else {
+                /* -l available (Task 2); implement here */
+                FILE *file_pointer = fopen(fname, "r");
+                if (file_pointer == NULL) {
+                    perror("fopen");
+                    exit(1);
+                }
+
+                char *buffer = malloc(buffer_size);
+                if (buffer == NULL) {
+                    perror("malloc");
+                    exit(1);
+                }
+
+                int n;
+                while ((n = fread(buffer, sizeof(char), buffer_size, file_pointer)) > 0) {
+                    if (fwrite(buffer, sizeof(char), n, stdout) != n) {
+                        perror("fwrite");
+                        exit(1);
+                    }
+                };
+
+                free(buffer);
+                if (fclose(file_pointer) == EOF) {
+                    perror("fclose");
+                    exit(1);
+                };
+            }
+        }
+    }
+    // すでに stdinに読み込まれているデータを stdout に書き出す処理が必要
+    if (size == 0) {
+        if (!use_stdio) {
             char *buffer = malloc(buffer_size);
             if (buffer == NULL) {
                 perror("malloc");
@@ -35,7 +102,7 @@ int do_cat(int size, char **args) {
             }
 
             int n;
-            while ((n = read(file_discripter, buffer, buffer_size)) > 0) {
+            while ((n = read(STDIN_FILENO, buffer, buffer_size)) > 0) {
                 if (write(STDOUT_FILENO, buffer, n) != n) {
                     perror("write");
                     exit(1);
@@ -43,18 +110,7 @@ int do_cat(int size, char **args) {
             };
 
             free(buffer);
-            if (close(file_discripter) == -1) {
-                perror("close");
-                exit(1);
-            };
         } else {
-            /* -l available (Task 2); implement here */
-            FILE *file_pointer = fopen(fname, "r");
-            if (file_pointer == NULL) {
-                perror("fopen");
-                exit(1);
-            }
-
             char *buffer = malloc(buffer_size);
             if (buffer == NULL) {
                 perror("malloc");
@@ -62,37 +118,14 @@ int do_cat(int size, char **args) {
             }
 
             int n;
-            while ((n = fread(buffer, sizeof(char), buffer_size, file_pointer)) > 0) {
+            while ((n = fread(buffer, sizeof(char), buffer_size, stdin)) > 0) {
                 if (fwrite(buffer, sizeof(char), n, stdout) != n) {
                     perror("fwrite");
                     exit(1);
                 }
             };
-
             free(buffer);
-            if (fclose(file_pointer) == EOF) {
-                perror("fclose");
-                exit(1);
-            };
         }
-    }
-    // すでに stdinに読み込まれているデータを stdout に書き出す処理が必要
-    if(size == 0){
-        char *buffer = malloc(buffer_size);
-        if (buffer == NULL) {
-            perror("malloc");
-            exit(1);
-        }
-
-        int n;
-        while ((n = read(STDIN_FILENO, buffer, buffer_size)) > 0) {
-            if (write(STDOUT_FILENO, buffer, n) != n) {
-                perror("write");
-                exit(1);
-            }
-        };
-
-        free(buffer);
     }
     return 0;
 }
